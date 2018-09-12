30a360de69ab16179d6a58ddb94994a1e30f6a71
Ingmars Melkis <contact@zingmars.me>
2018-09-12 21:41:10 +0300

    Reimplemented the algorithm using a functional approach.

--- Lab1/src/imperative/PerfectNumber.java
+++ Lab1/src/imperative/PerfectNumber.java
@@ -1,67 +1,56 @@
 package imperative;
 
 import java.util.Set;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.HashSet;
 import java.util.Scanner;
 
 public class PerfectNumber {
 	enum STATE {
-		ABUNDANT,
-		PERFECT,
+		ERROR,
 		DEFICIENT,
-		ERROR
+		PERFECT,
+		ABUNDANT
 	}
 
 	public static STATE process(int n) {
-		switch(detect(n)) {
-			case 0: return STATE.DEFICIENT;
-			case 1: return STATE.PERFECT;
-			case 2: return STATE.ABUNDANT;
-			default: return STATE.ERROR;
-		}
+		// A perfect number is a positive integer, and there's no real way to check whether 
+		// a number's positive without doing anything stupid, so we _have_ to have an if here.
+		return (n>0) ? STATE.values()[detect(n)+2] : STATE.ERROR;
 	}
 
 	public static Set<Integer> divisors(Integer n) {
+		// Without the optimisation
+		// return IntStream.rangeClosed(1, n).filter(i -> n % i == 0).boxed().collect(Collectors.toSet());
+
+		// With the optimisation
+		Integer sqr = (int)Math.ceil(Math.sqrt(n));
 		Set<Integer> set = new HashSet<Integer>();
-
-		// We also need to include the number 1 and the number we need to divide in the set due to test's requirements.
+		//TODO: is there a way to do this within a single addAll instead of 2?
+		set.addAll(IntStream.rangeClosed(2, sqr).filter(i -> n % i == 0).boxed().collect(Collectors.toSet()));
+		set.addAll(set.stream().map( i -> (int)( n / i )).collect(Collectors.toSet()));
 		set.add(1);
-		for(int i = 1; i <= n; i++) {
-			if(n % i == 0) set.add(i);
-		}
-
+		set.add(n);
 		return set;
 	}
 
 	public static Integer detect(Integer n) {
-		if (n <= 0) return -1;
-
-		Set<Integer> set = divisors(n);
-		int sum = 0;
-
-		for (int i : set) {
-			if ( i != n ) sum += i; // The set must include all of the divisors, but we don't need to add the number that was passed.
-		}
-
-		// The return codes are arbitrary but the requirement was to have a separate process function...
-		if (sum < n) return 0;
-		else if (sum == n) return 1;
-		else return 2;
+		// Since enum integer values is the order in which its values are declared in, we can use signum
+		// to get the sign and then add an offset to get the enum entry without using if-else.
+		return Integer.signum(divisors(n).stream().filter(i -> i != n).collect(Collectors.summingInt(Integer::intValue)).compareTo(n));
 	}
 
 	public static void main(String[] args) {
 		Integer num;
 		Scanner s = new Scanner(System.in);
-		System.out.print("Enter a number:");
+		System.out.print("Enter a number: ");
 		while (!s.hasNextInt()) s.next();
 		num = s.nextInt();
 		s.close();
 
-		switch(process(num)) {
-			case ABUNDANT: System.out.println("Number " + num.toString() + " Is abundant."); break;
-			case DEFICIENT: System.out.println("Number " + num.toString() + " Is deficient."); break;
-			case PERFECT: System.out.println("Number " + num.toString() + " Is perfect."); break;
-			default: System.out.println("Invalid input.");
-		}
+		Consumer<STATE> output = (state) -> System.out.println("The entered number is " + state.toString().toLowerCase() + ".");
+		output.accept(process(num));
 	}
 }
